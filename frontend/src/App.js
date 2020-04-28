0643ce11018e233d2e9e8ce262d9b416d41881f2
manaswebrepo <mymanas@gmail.com>
2020-04-29 03:54:00 +0530

    navigation, react portal, transition CSS, drawer menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,21 +5,25 @@ import {
   Redirect,
   Switch,
 } from "react-router-dom";
-import Users  from "./user/pages/Users";
+import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "../src/shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path='/' exact>
-          <Users />
-        </Route>
-        <Route path='/places/new' exact>
-          <NewPlace />
-        </Route>
-        <Redirect to='/' />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path='/' exact>
+            <Users />
+          </Route>
+          <Route path='/places/new' exact>
+            <NewPlace />
+          </Route>
+          <Redirect to='/' />
+        </Switch>
+      </main>
     </Router>
   );
 };
